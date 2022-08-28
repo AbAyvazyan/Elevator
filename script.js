9cbc89c9469e0cc427a0b0db2d6bbc1ab30f7a46
AbAyvazyan333 <abayvazyan333@gmail.com>
2022-08-28 18:05:55 +0400

    almost ready

--- script.js
+++ script.js
@@ -6,9 +6,16 @@ const closeDoor = document.getElementsByClassName("close")[0]
 const floorNums = document.getElementsByClassName("floor-number")
 const lift = document.getElementsByClassName('lift')[0]
 let floorArray = []
+let arrayForAwhile = []
 let nowFloor = 1
+let onMove = false
 
 function doorClose() {
+
+    if (onMove){
+        return false
+    }
+
     return new Promise((res,rej)=>{
         let marginLeftValue = parseInt(window.getComputedStyle(rightDoor).marginLeft)
 
@@ -17,6 +24,7 @@ function doorClose() {
                 marginLeftValue--
                 rightDoor.style.marginLeft = marginLeftValue + "px"
             }else{
+                res()
                 clearInterval(id)
             }
         }, 10)
@@ -26,6 +34,11 @@ function doorClose() {
 }
 
 function doorOpen(){
+
+    if (onMove){
+        return false
+    }
+
     return new Promise((res,rej)=> {
         let marginLeftValue = parseInt(window.getComputedStyle(rightDoor).marginLeft)
 
@@ -34,6 +47,7 @@ function doorOpen(){
                 marginLeftValue++
                 rightDoor.style.marginLeft = marginLeftValue + "px"
             } else {
+                res()
                 clearInterval(id)
             }
         }, 10)
@@ -45,43 +59,90 @@ openDoor.addEventListener("click",()=>doorOpen())
 closeDoor.addEventListener("click",()=>doorClose())
 
 for (let i = 0; i < floorNums.length; i++) {
-    floorNums[i].addEventListener("click",function (){
-        if (!floorArray.includes(floorNums[i].innerText)){
-            floorArray.push(floorNums[i].innerText)
-            floorArray.sort()
+    floorNums[i].addEventListener("click",function () {
+
+        if (!onMove || (onMove && floorArray[floorArray.length-1]-this.innerText<nowFloor+this.innerText)) {
+            if (!floorArray.includes(floorNums[i].innerText) && nowFloor !== this.innerText) {
+
+                if (nowFloor < this.innerText) {
+                    floorArray.push(this.innerText)
+                    floorArray.sort()
+                } else {
+                    floorArray.push(this.innerText)
+                    floorArray.sort((a, b) => b - a)
+                }
+            }
+        }else{
+    if (!arrayForAwhile.includes(floorNums[i].innerText) && nowFloor !== this.innerText) {
+
+        if (this.innerText < floorArray[floorArray.length-1] && nowFloor < this.innerText) {
+            arrayForAwhile.push(this.innerText)
+            arrayForAwhile.sort()
+        } else {
+            arrayForAwhile.push(this.innerText)
+            arrayForAwhile.sort((a, b) => b - a)
         }
 
+    }
+}
     })
 }
+
 
 let bottom = parseInt(window.getComputedStyle(lift).bottom)
 function changeFloor(number){
+
     return new Promise((res,rej)=>{
 
-        if(nowFloor<number){
-            const up = setInterval(function (){
-                if (bottom <=number*11){
+    if(nowFloor < number)
+        {
+            onMove = true
+            const up = setInterval(function () {
+                if (bottom <= (number - 1) * 12.5) {
                     bottom++
                     lift.style.bottom = bottom + "vh"
-                }else{
+                } else {
+                    nowFloor = +number
+                    onMove = false
+                    res()
                     clearInterval(up)
                 }
-            },50)
+            }, 50)
         }
 
-        if (nowFloor>number){
-            const down = setInterval(function (){
-                if (bottom >=number*11){
+        if (nowFloor > number) {
+            onMove = true
+            const down = setInterval(function () {
+                if (bottom >= (number - 1) * 12.5) {
                     bottom--
                     lift.style.bottom = bottom + "vh"
-                }else{
+                } else {
+                    nowFloor = +number
+                    onMove = false
+                    res()
                     clearInterval(down)
                 }
-            },50)
+            }, 50)
         }
-        nowFloor = number
-    })
 
+    })
 }
 
-changeFloor(7)
+startMovingButton.addEventListener("click", function ab(){
+
+    if (onMove){
+        return false
+    }
+
+
+     changeFloor(floorArray.shift()).then(()=> {
+         doorOpen().then(()=>{
+             doorClose().then(()=>{
+
+                 floorArray.push(...arrayForAwhile)
+                 arrayForAwhile = []
+                 ab()
+             })
+         })
+     })
+})
